cb1bdedb8d27d75ae7862725539eeefaee74b27e
Giabao <giabao2344@gmail.com>
2026-04-14 15:05:06 +0700

    update

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -26,9 +26,25 @@ int extended_euclid(int a, int b, int &x, int &y) {
     return g;
 }
 
-int mod_inverse(int a, int m) {
-    int x, y;
-    int g = extended_euclid(a, m, x, y);
+long long extended_euclid(long long a, long long b, long long &x, long long &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+
+    long long x1, y1;
+    long long g = extended_euclid(b, a % b, x1, y1);
+
+    x = y1;
+    y = x1 - (a / b) * y1;
+
+    return g;
+}
+
+long long mod_inverse(long long a, long long m) {
+    long long x, y;
+    long long g = extended_euclid(a, m, x, y);
 
     if (g != 1) return -1;
 
